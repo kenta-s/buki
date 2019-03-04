e407b0aecfe97c62408c2bfbad1da1d5b91ea4dc
kenta-s <knt01222@gmail.com>
2019-03-04 22:39:04 +0900

    UnionFind

---
+++ ruby/union_find.rb
@@ -0,0 +1,78 @@
+class UnionFind
+  attr :parent
+
+  def initialize(nodes)
+    @parent = {}
+    nodes.each do |node|
+      parent[node] = -1
+    end
+  end
+
+  def root(x)
+    if parent[x] < 0
+      x
+    else
+      parent[x] = root(parent[x])
+    end
+  end
+
+  def size(x)
+    return -parent[root(x)]
+  end
+
+  def connect(x, y)
+    rx = root(x)
+    ry = root(y)
+    if rx == ry
+      return false
+    else
+      if size(rx) < size(ry)
+        rx, ry = ry, rx
+      end
+
+      parent[rx] += parent[ry]
+      parent[ry] = rx
+
+      return true
+    end
+  end
+end
+
+require 'minitest/autorun'
+
+class TestUnionFind < Minitest::Test
+  def test_root
+    uf = UnionFind.new([1,2,3,4])
+    assert_equal 1, uf.root(1)
+    assert_equal 2, uf.root(2)
+    assert_equal 3, uf.root(3)
+    assert_equal 4, uf.root(4)
+  end
+
+  def test_size
+    uf = UnionFind.new([1,2,3,4])
+    assert_equal 1, uf.size(1)
+    assert_equal 1, uf.size(2)
+    assert_equal 1, uf.size(3)
+    assert_equal 1, uf.size(4)
+  end
+
+  def test_connect
+    uf = UnionFind.new([1,2,3,4])
+    uf.connect(1,2)
+    assert_equal 1, uf.root(1)
+    assert_equal 1, uf.root(2)
+    assert_equal 3, uf.root(3)
+    assert_equal 2, uf.size(2)
+    assert_equal 1, uf.size(3)
+    assert_equal 1, uf.size(4)
+
+    uf.connect(3,4)
+    assert_equal 3, uf.root(4)
+    assert_equal 2, uf.size(3)
+
+    uf.connect(1,4)
+    assert_equal 1, uf.root(4)
+    assert_equal 4, uf.size(3)
+  end
+end
